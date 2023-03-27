18986ae2f45991df49a61e0cfc8d9bffb9e8006a
¨Brisa <ahmygoddes@outlook.es>
2023-03-26 22:22:36 -0300

    Otros campos definidos/ submit/ validación datos en consola momentaneamente

--- src/sections/Contacts/Contacts.jsx
+++ src/sections/Contacts/Contacts.jsx
@@ -1,10 +1,16 @@
+
 import { Field, Form, Formik } from "formik";
 
 function Contact() {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <section id="Contacts" className="container my-5">
       <h2>Contacts</h2>
       <Formik
+        onSubmit={handleSubmit}
         initialValues={{
           name: "",
           email: "",
@@ -31,15 +37,39 @@ function Contact() {
           return errors;
         }}
       >
-        {({ values, touched, errors }) => (
+        {({ touched, errors, isValid }) => (
           <Form>
             <div className="d-flex flex-column">
-              <label>Nombre</label>
+              <label>Name</label>
               <Field name="name" type="text" />
               {touched.name && errors.name && (
                 <p className="text-danger my-1">{errors.name}</p>
               )}
             </div>
+            <div className="d-flex flex-column">
+              <label>Email</label>
+              <Field name="email" type="text" />
+              {touched.email && errors.email && (
+                <p className="text-danger my-1">{errors.email}</p>
+              )}
+            </div>
+            <div className="d-flex flex-column">
+              <label>Phone</label>
+              <Field name="phone" type="text" />
+              {touched.phone && errors.phone && (
+                <p className="text-danger my-1">{errors.phone}</p>
+              )}
+            </div>
+            <div className="d-flex flex-column">
+              <label>Message</label>
+              <Field name="message" type="text" as="textarea" />
+              {touched.message && errors.message && (
+                <p className="text-danger my-1">{errors.message}</p>
+              )}
+            </div>
+            <button type="submit" disabled={!isValid}>
+              Send
+            </button>
           </Form>
         )}
       </Formik>
