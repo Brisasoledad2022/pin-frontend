454bd5a450b1c416354e43564ac381b552b4c2e0
Pablo de Marcos <pademarcos@gmail.com>
2023-03-28 20:36:03 -0300

    Anclas convertidas en botones

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -3,12 +3,27 @@ import logo from "../../assets/logo.png";
 const Navbar = () => {
   const menuItems = ["Home", "About", "Products", "Services", "Contacts"];
 
+  const clickHandler = id => {
+    document.getElementById(id).scrollIntoView({behavior: 'smooth'})
+  }
+
   return (
+    // <header className="d-flex align-items-center w-100 bg-transparent justify-content-between py-5 container">
     <header className="d-flex align-items-center w-100 bg-transparent justify-content-between py-5 container">
       <img className="logo" alt="logo" src={logo} />
 
       <div className="d-flex align-items-center ">
-        {menuItems.map(item => <a className="mx-3" key={item} href={`#${item}`}>{item}</a>)}
+        {/* {menuItems.map(item => <a className="mx-3" key={item} href={`#${item}`}>{item}</a>)} */}
+        {menuItems.map(item => ( 
+          <button 
+            name={item} 
+            key={item} 
+            className='mx-3 bg-transparent border-0' 
+            onClick={e => clickHandler(e.target.name)}
+          >
+            {item}
+          </button>)
+          )}
       </div>
     </header>
   );
