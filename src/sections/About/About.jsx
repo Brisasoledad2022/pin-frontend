4643a59be32324276fc3ffee664b00569fd81bf8
¨Brisa <ahmygoddes@outlook.es>
2023-04-16 16:28:18 -0300

    about responsive

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,7 +1,8 @@
-
+import useWindowInfo from "../../hooks/useWindowInfo";
 import React from "react";
 
 export default function About() {
+  const {mobileDesign} = useWindowInfo()  
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center w-100 p-5 mt-4 position-relative"
@@ -12,12 +13,12 @@ export default function About() {
         <h1 className="m-0 fw-bold text-orange neon">-Drinks & Brunch-</h1>
 
       </div>
-      <p className="m-0 w-50 lh-lg text-gray-300 lh-1 text-justify">
+      <p className={`m-0 ${mobileDesign ? "w-100" : "w-50"} lh-lg text-gray-300 lh-1 text-justify`}>
       "The Bloom" is a luxurious bar that offers a unique and elegant experience to its patrons. The bar is adorned with beautiful flower arrangements that create a vibrant and inviting atmosphere. The menu features a range of exquisite cocktails and drinks that are expertly crafted by skilled mixologists. The staff are friendly and attentive, providing exceptional service to ensure that guests feel pampered and indulged. Whether you're looking for a sophisticated night out or a relaxing after-work drink, "The Bloom" is the perfect destination for anyone who appreciates fine decor, exquisite drinks, and exceptional service.
       </p>
 
       {/* Orange line on the left */}
-      <div className="bg-orange position-absolute start-0 h-80 vw-2"></div>
+      <div className={`bg-orange position-absolute start-0 h-80 vw-2 ${!mobileDesign && "d-none"}`}></div>
       {/* Orange line on the left */}
     </div>
   );
